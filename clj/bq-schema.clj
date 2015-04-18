0a35df4feae728f8951005406beae8ac614b0f80
Alex Bird <alexebird@gmail.com>
2015-04-18 15:59:14 -0700

    initial

---
+++ clj/bq-schema.clj
@@ -0,0 +1,50 @@
+(ns bq.schema
+  (:require [cheshire.core :refer :all]
+            [clj-time.core :as t]
+            [clj-time.format :as f]
+            [clojure.pprint :refer (pprint)]
+            [clojure.string :refer (join)]))
+
+(def raw-doc (parse-string (slurp "../luffer/tmp/one.json")))
+
+(def fmt (f/formatters :date-time-no-ms))
+
+(defn- datey? [v]
+  (re-matches #"\d{4}-\d{2}-\d{2}.*" v))
+
+(defn- detect-type [v]
+  (cond
+   (nil? v) "STRING"
+   (or (instance? Boolean v)) "BOOLEAN"
+   (or (instance? Double v)) "FLOAT"
+   (or (instance? Long v) (instance? Integer v)) "INTEGER"
+   (or (vector? v) (map? v) (list? v)) "RECORD"
+   (datey? v)           "TIMESTAMP" 
+   (instance? String v) "STRING"))
+
+;; (defn- schema-field [keys type]
+;;   (join ":" [(join "." keys) type]))
+
+(declare mark-types)
+
+(defn- schema-field [k v]
+  (let [json {"name" k
+              "type" (detect-type v)}]
+    (if (map? v)
+      (merge json {"fields" (mark-types v)})
+      json)))
+
+(defn mark-types [hsh]
+  (vec (map (fn [[k v]]
+              (schema-field k v))
+            hsh)))
+
+(defn make-schema [json]
+  (join "," (mark-type [] json)))
+
+
+;; (defn mark-type [json]
+;;   (map
+;;    (cond
+;;     (detect-type v))
+;;    json))
